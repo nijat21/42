6ebe0eba6a92cfda3150cffb39b683702f99f12a
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-01 00:14:45 +0100

    ex12 wip

---
+++ c02/ex12/ft_print_memory.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+char **malloc(int rows)
+{
+    int max_length;
+    char str[rows];
+
+    max_length = 16;
+    str = malloc()
+}
+
+void *ft_print_memory(void *addr, unsigned int size)
+{
+    char *str;
+    int i;
+
+    if (size == 0)
+        return (0);
+    while (str[i])
+    {
+        if (!(32 <= str[i] && str[i] <= 126))
+            str[i] = ".";
+    }
+
+    return addr;
+}
+
+int main(void)
+{
+    char **str =
+        return (0);
+}
