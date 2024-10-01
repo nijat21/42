d86c9e604396b7b8dba78b0a16dc6d5633dd9918
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-01 01:20:08 +0100

    .

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -1,21 +1,15 @@
 #include <unistd.h>
 
-char **malloc(int rows)
-{
-    int max_length;
-    char str[rows];
-
-    max_length = 16;
-    str = malloc()
-}
-
 void *ft_print_memory(void *addr, unsigned int size)
 {
+    int max_length;
     char *str;
     int i;
 
+    max_length = 16;
     if (size == 0)
         return (0);
+    i = 0;
     while (str[i])
     {
         if (!(32 <= str[i] && str[i] <= 126))
---
+++ c02/ex12/test.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+int main(void)
+{
+    char *str = "Sometimes tests are useful";
+
+    int i = 0;
+    while
+
+        return (0);
+}
