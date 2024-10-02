1b3528fbe0905cc64f123f78aa92853df3617c27
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-02 01:10:16 +0100

    ex00 done

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,47 @@
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    if (!s1[i] && !s2[i])
+        return (0);
+    while (s1[i] || s2[i])
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s1 = "something";
+    char *s2 = "some";
+
+    char *s3 = "some";
+    char *s4 = "soms";
+
+    char *s5 = "some";
+    char *s6 = "some";
+
+    char *s7 = "";
+    char *s8 = "";
+
+    printf("Original: %i\n", strcmp(s1, s2));
+    printf("Mine: %i\n", ft_strcmp(s1, s2));
+
+    printf("Original: %i\n", strcmp(s3, s4));
+    printf("Mine: %i\n", ft_strcmp(s3, s4));
+
+    printf("Original: %i\n", strcmp(s5, s6));
+    printf("Mine: %i\n", ft_strcmp(s5, s6));
+
+    printf("Original: %i\n", strcmp(s7, s8));
+    printf("Mine: %i\n", ft_strcmp(s7, s8));
+
+    return (0);
+}
