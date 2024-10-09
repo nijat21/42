01a3a76cdfa2eb0e0c068feed353b2f3af924161
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-09 21:05:37 +0100

    ex04 done

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-
 int ft_fibonacci(int index)
 {
     if (index < 0)
         return (-1);
-    if (index == 0)
-        return (0);
-    printf("Index: %i\n", index);
-
-    return ((index - 1) + ft_fibonacci(index - 2));
+    if (index < 2)
+        return index;
+    return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
+// fib(6)
+// fib(5) + fib(4)
+// fib(1) + fib(0)
+/* #include <stdio.h>
 
 int main()
 {
-    int res = ft_fibonacci(3);
+    int res = ft_fibonacci(2); // 0, 1, 1, 2, 3, 5, 8, 13
     printf("Result: %i\n", res);
 
     return (0);
-}
+} */
